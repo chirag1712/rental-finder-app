2b643bcea62b51b9ea0e84c91a03489c9f9ae144
chirag1712 <chiragjindal1712@gmail.com>
2021-05-23 02:55:41 +0530

    modified the server file to include the student endpoints

--- server/server.js
+++ server/server.js
@@ -1,9 +1,15 @@
 const express = require('express');
-const http = require('http');
-const mySql = require('mysql');
 
 const app = express();
-const server = http.createServer(app);
 
+// simple route
+app.get("/", (_, res) => {
+    res.json({ message: "Hello, welcome to our CS348 project." });
+});
+
+// require all routes here
+require("./app/routes/student.routes.js")(app);
+
+// listening for requests
 const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => console.log(`Server running on ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
