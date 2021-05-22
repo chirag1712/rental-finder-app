7896fd8606c8dee83dccc91b04c6ab9405aed93b
uday <usbenipa@uwaterloo.ca>
2021-05-22 19:25:43 -0400

    frontend created

--- client/src/App.js
+++ client/src/App.js
@@ -4,11 +4,16 @@ import axios from 'axios';
 
 function App() {
   const [studentInfo, setStudentInfo] = useState({
+<<<<<<< HEAD
     email: '', name: ''
+=======
+    studentEmail: '', studentName: ''
+>>>>>>> 8de9310... frontend created
   });
 
   const [studentList, setStudentList] = useState([]);
 
+<<<<<<< HEAD
   useEffect(() => {
     const func = async () => {
       const res = await axios.get('/students');
@@ -18,6 +23,17 @@ function App() {
 
     func()
   }, []);
+=======
+  // useEffect(() => {
+  //   const func = async () => {
+  //     const res = await axios.get('/students');
+  //     //needs to be checked once DB is setup
+  //     setStudentList([...res.data]);
+  //   }
+
+  //   func()
+  // }, []);
+>>>>>>> 8de9310... frontend created
 
   const onChange = (e) => setStudentInfo({...studentInfo, [e.target.name]: e.target.value});
 
@@ -25,11 +41,19 @@ function App() {
 
     const info = {...studentInfo};
 
+<<<<<<< HEAD
     try {
       const res = await axios.post('/students', info);
     } catch(err) {
       console.log(err.response.data);
     }
+=======
+    // try {
+    //   const res = await axios.post('/students', info);
+    // } catch(err) {
+    //   console.log(err.response.data);
+    // }
+>>>>>>> 8de9310... frontend created
     setStudentList([...studentList, info]);
   };
 
@@ -39,23 +63,39 @@ function App() {
       <input 
         className='input'
         type='text'
+<<<<<<< HEAD
         name='email'
         placeholder='Student Email'
         value={studentInfo.email}
+=======
+        name='studentEmail'
+        placeholder='Student Email'
+        value={studentInfo.studentEmail}
+>>>>>>> 8de9310... frontend created
         onChange={e => onChange(e)}/>
       <input 
         className='input'
         type='text'
+<<<<<<< HEAD
         name='name'
         placeholder='Full Name'
         value={studentInfo.name}
+=======
+        name='studentName'
+        placeholder='Full Name'
+        value={studentInfo.studentName}
+>>>>>>> 8de9310... frontend created
         onChange={e => onChange(e)}/>
       <button className="btn" onClick={onSubmit} >Submit</button>
       <br/>
       <br/>
       <ul>
       {
+<<<<<<< HEAD
         studentList.map(student => <li>{student.name}</li>)
+=======
+        studentList.map(student => <li>{student.studentName}</li>)
+>>>>>>> 8de9310... frontend created
       }
       </ul>
     </div>
