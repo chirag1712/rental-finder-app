caf34d354b9228ed3b7422be8ffe4d6d2a48682c
Uday Singh Benipal <77290363+UdayBenipal@users.noreply.github.com>
2021-05-24 15:31:21 -0400

    frontend created (#4)
    * frontend created
    
    * post and get request working
    
    * frontend created
    
    * merge conflicts solved
    
    * fixed a small bug in the frontend
    
    * removed console log
    
    Co-authored-by: Parmeet Chawla <ps3chawl@uwaterloo.ca>

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,64 @@
 import './App.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [studentInfo, setStudentInfo] = useState({
+    email: '', name: ''
+  });
+
+  const [studentList, setStudentList] = useState([]);
+
+  useEffect(() => {
+    const func = async () => {
+      const res = await axios.get('/students');
+      //needs to be checked once DB is setup
+      setStudentList([...res.data]);
+    }
+
+    func()
+  }, []);
+
+  const onChange = (e) => setStudentInfo({...studentInfo, [e.target.name]: e.target.value});
+
+  const onSubmit = async () => {
+
+    const info = {...studentInfo};
+
+    try {
+      const res = await axios.post('/students', info);
+    } catch(err) {
+      console.log(err.response.data);
+    }
+    
+    setStudentList([...studentList, info]);
+  };
+
   return (
     <div className="App">
-      <h1>Basic set up done!!</h1>
+
+      <input 
+        className='input'
+        type='text'
+        name='email'
+        placeholder='Student Email'
+        value={studentInfo.email}
+        onChange={e => onChange(e)}/>
+      <input 
+        className='input'
+        type='text'
+        name='name'
+        placeholder='Full Name'
+        value={studentInfo.name}
+        onChange={e => onChange(e)}/>
+      <button className="btn" onClick={onSubmit} >Submit</button>
+      <br/>
+      <br/>
+      <ul>
+      {
+        studentList.map(student => <li>{student.name}</li>)
+      }
+      </ul>
     </div>
   );
 }
