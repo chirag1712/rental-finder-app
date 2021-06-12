d4f4ed90a18ef293bbdb30a3abd9b1458e323b72
Rain Zhao <rainzhao2000@gmail.com>
2021-06-12 17:05:53 -0400

    rain fix (#20)

--- server/app/config/db.config.js
+++ server/app/config/db.config.js
@@ -1,22 +1,20 @@
-module.exports = function(){
+module.exports = (() => {
     switch(process.env.NODE_ENV){
         case 'dev':
             return {
-              HOST: process.env.LOCAL_DB_HOST,
-              USER: process.env.LOCAL_DB_USER,
-              PASSWORD: process.env.LOCAL_DB_PASSWORD,
-              DB: process.env.DB_NAME
+                HOST: process.env.LOCAL_DB_HOST,
+                USER: process.env.LOCAL_DB_USER,
+                PASSWORD: process.env.LOCAL_DB_PASSWORD,
+                DB: process.env.DB_NAME
             };
-
         case 'prod':
             return {
-              HOST: process.env.RDS_DB_HOST,
-              USER: process.env.RDS_DB_USER,
-              PASSWORD: process.env.RDS_DB_PASSWORD,
-              DB: process.env.DB_NAME
+                HOST: process.env.RDS_DB_HOST,
+                USER: process.env.RDS_DB_USER,
+                PASSWORD: process.env.RDS_DB_PASSWORD,
+                DB: process.env.DB_NAME
             };
-
         default:
             throw "Please set NODE_ENV in server/.env correctly"
     }
-};
+})();
--- server/database/db_import.js
+++ server/database/db_import.js
@@ -1,9 +1,10 @@
 const { exec_sql_script } = require('./db_helper.js');
+const { DB } = require('../app/config/db.config.js');
 
 if (process.argv.length != 3) {
     console.error(`Usage: ${process.argv[0]} ${process.argv[1]} <file_path>`);
 } else {
     exec_sql_script('database/migrations/drop_db.sql')
     .then(() => exec_sql_script('database/migrations/create_db.sql'))
-    .then(() => exec_sql_script(process.argv[2], [process.env.DB_NAME]));
+    .then(() => exec_sql_script(process.argv[2], [DB]));
 }
