a405aa9aed911b2bf3151d7814d7d87a03e37048
Parmeet Chawla <ps3chawl@uwaterloo.ca>
2021-06-13 13:23:08 -0400

    basic create posting structure

---
+++ server/app/controllers/posting.controller.js
@@ -0,0 +1,117 @@
+//const User = require("../models/user.model.js");
+const Posting = require("../models/posting.model.js");
+const Address = require("../models/posting.model.js");
+const AddressOf = require("../models/posting.model.js");
+
+// Create a new posting
+exports.create = async (request, result) => {
+  // Validate request
+  if (!request.body) {
+    result.status(400).send({
+      message: "Content can not be empty!",
+    });
+  }
+
+  // Create a Posting
+  const posting = new Posting({
+    user_id: request.body.user_id,
+    term: request.body.term,
+    start_date: request.body.start_date,
+    end_date: request.body.end_date,
+    pop: 0,
+    price_per_month: request.body.price_per_month,
+    gender_details: request.body.gender_details,
+    rooms_available: request.body.rooms_available,
+    total_rooms: request.body.total_rooms,
+    description: request.body.description,
+    created_at: Date.now(),
+    updated_at: Date.now(),
+  });
+
+  // Create a Address (only if it doesn't exist)
+  const address = new Address({
+    street_num: request.body.street_num,
+    street_name: request.body.street_name,
+    city: request.body.city,
+    postal_code: request.body.postal_code,
+    building_name: request.body.building_name,
+  });
+
+  // validate that user exists in the db
+  Posting.userCheck(request.body.user_id, (err, userExist) => {
+    if (err) {
+      result.status(500).send({
+        message:
+          err.message ||
+          "Some error occurred while checking existence of User.",
+      });
+    } else if (!userExist) {
+      result.status(400).json({ error: "User doesn't exist." });
+      return;
+    }
+  });
+
+  // Save Posting in the database
+  Posting.createPosting(Posting, (err, posting) => {
+    if (err) {
+      return result.status(500).send({
+        message:
+          err.message || "Some error occurred while creating the posting.",
+      });
+    }
+
+    // Search Address in the database
+    Address.search(Address, (err, address) => {
+      if (err) {
+        return result.status(500).send({
+          message:
+            err.message || "Some error occurred while searching the address.",
+        });
+      }
+
+      const address_of = new AddressOf({
+        posting_id: posting.posting_id,
+        address_id: address.address_id,
+      });
+
+      if (address[0]) {
+        // address exists
+
+        //join address and posting
+        AddressOf.createAddressOf(address_of, (err, address) => {
+          if (err)
+            result.status(500).send({
+              message:
+                err.message ||
+                "Some error occurred while joining the address and posting.",
+            });
+          else result.send(address);
+        });
+      } else {
+        // Save Address in the database
+        Address.createAddress(address, (err, address) => {
+          if (err)
+            result.status(500).send({
+              message:
+                err.message || "Some error occurred while saving the address.",
+            });
+          else result.send(address);
+        });
+
+        //join address and posting
+
+        AddressOf.createAddressOf(address_of, (err, data) => {
+          if (err)
+            result.status(500).send({
+              message:
+                err.message ||
+                "Some error occurred while joining the address and posting.",
+            });
+          else result.send(data);
+        });
+      }
+    });
+
+    result.send(posting);
+  });
+};
---
+++ server/app/models/posting.model.js
@@ -0,0 +1,124 @@
+const sql = require("./db.js");
+
+// constructor
+const Posting = function (posting) {
+  this.user_id = posting.user_id;
+  this.term = posting.term;
+  this.start_date = posting.start_date;
+  this.end_date = posting.end_date;
+  this.pop = 0;
+  this.price_per_month = posting.price_per_month;
+  this.gender_details = posting.gender_details;
+  this.rooms_available = posting.rooms_available;
+  this.total_rooms = posting.total_rooms;
+  this.description = posting.description;
+
+  // check how to implement these
+  this.created_at = Date.now();
+  this.updated_at = Date.now();
+};
+
+const Address = function (address) {
+  this.street_num = address.street_num;
+  this.street_name = address.street_name;
+  this.city = address.city;
+  this.postal_code = address.postal_code;
+  this.building_name = address.building_name;
+};
+
+const AddressOf = function (addressof) {
+  this.posting_id = addressof.posting_id;
+  this.address_id = addressof.address_id;
+};
+
+//check if user_id exists in database
+Posting.userCheck = (user_id, result) => {
+  sql.query(
+    "SELECT count(*) FROM User WHERE user_id = ? GROUP BY user_id",
+    user_id,
+    (err, res) => {
+      if (err) {
+        console.log("error: ", err);
+        result(null, err);
+        return;
+      }
+      if (res == 1) {
+        result(null, true);
+        console.log("User exists");
+        return;
+      } else {
+        console.log("User doesnt exists");
+        result(null, false);
+        return;
+      }
+    }
+  );
+};
+
+Posting.createPosting = (newPosting, result) => {
+  sql.query("INSERT INTO Posting SET ?", newPosting, (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      result(err, null);
+      return;
+    }
+
+    console.log("created posting: ", {
+      posting_id: res.insertId,
+      ...newPosting,
+    });
+    result(null, { posting_id: res.insertId, ...newPosting });
+  });
+};
+
+Address.createAddress = (newAddress, result) => {
+  sql.query("INSERT INTO Address SET ?", newAddress, (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      result(err, null);
+      return;
+    }
+
+    console.log("created address: ", {
+      address_id: res.insertId,
+      ...newAddress,
+    });
+    result(null, { address_id: res.insertId, ...newAddress });
+  });
+};
+
+AddressOf.createAddressOf = (newAddressOf, result) => {
+  sql.query("INSERT INTO AddressOf SET ?", newAddressOf, (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      result(err, null);
+      return;
+    }
+
+    console.log("created new addressof translation: ", {
+      ...newAddressOf,
+    });
+    result(null, true);
+  });
+};
+
+Address.search = (address, result) => {
+  sql.query(
+    "SELECT * FROM Address WHERE street_num = " +
+      address.street_num +
+      " AND street_name = " +
+      address.street_name,
+    (err, res) => {
+      if (err) {
+        console.log("error: ", err);
+        result(null, err);
+        return;
+      }
+
+      console.log("address found: ", res);
+      result(null, res);
+    }
+  );
+};
+
+module.exports = Posting;
