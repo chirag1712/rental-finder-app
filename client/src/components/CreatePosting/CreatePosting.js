b51b41d11ad6fffaaa56762a9b5e668b34e34ab6
Anne Hu <annehuu2@gmail.com>
2021-06-13 15:30:55 -0400

    Added post request for create postings

--- client/src/components/CreatePosting/CreatePosting.js
+++ client/src/components/CreatePosting/CreatePosting.js
@@ -1,41 +1,101 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Wrapper, Header } from './CreatePostingStyles';
+import axios from 'axios';
+import { render } from 'react-dom';
 
-const CreatePosting = (props) => {
-  return (
-    <Wrapper>
-      <Header> Create Posting! </Header>
+class CreatePosting extends Component {
+  state = {
+    user_id: '1',
+    term: '',
+    start_date: '',
+    end_date: '',
+    price_per_month: '',
+    gender_details: '',
+    rooms_available: '',
+    total_rooms: '',
+    description: '',
+    street_num: '',
+    street_name: '',
+    city: '',
+    postal_code: ''
+  }
 
-      <form>
-        <label for='address'> Address: </label>
-        <input name='address' type='text' />
-        <br></br>
+  handleChange = event => {
+    this.setState({ [event.target.name]: event.target.value })
+  }
 
-        <label for="term">Choose Your Term: </label>
-        <select name="term" id="term">
-          <option value="Fall">Fall</option>
-          <option value="Winter">Winter</option>
-          <option value="Spring">Spring</option>
-        </select>
-        <br></br>
+  handleSubmit = event => {
+    event.preventDefault();
 
-        <label for="start">Start Date: </label>
-        <input type="date" id="start" name="start-date"
-          value="2021-09-01"
-          min="2021-05-01" max="2022-08-31"></input>
-        <br></br>
+    const data = {
+      user_id: this.state.user_id,
+      term: this.state.term,
+      start_date: this.state.start_date,
+      end_date: this.state.end_date,
+      price_per_month: this.state.price_per_month,
+      gender_details: this.state.gender_details,
+      rooms_available: this.state.rooms_available,
+      total_rooms: this.state.total_rooms,
+      description: this.state.description,
+      street_num: this.state.street_num,
+      street_name: this.state.street_name,
+      city: this.state.city,
+      postal_code: this.state.postal_code
+    };
 
-        <label for="start">End Date: </label>
-        <input type="date" id="end" name="end-start"
-          value="2021-12-31"
-          min="2021-05-01" max="2022-08-31"></input>
-        <br></br>
+    axios.post('http://localhost:5000/create', { data })
+      .then(res => {
+        console.log(data);
+      })
+  }
 
-        <label for='price'> Price Per Month: </label>
-        <input name='price' type='text' />
-        <br></br>
+  render() {
+    return (
+      <Wrapper>
+        <Header> Create Posting! </Header>
 
-        {/* 
+        <form action="" onSubmit={this.handleSubmit}>
+          <label for='street_num'> Street Number: </label>
+          <input name='street_num' type='text' onChange={this.handleChange} />
+          <br></br>
+
+          <label for='street_name'> Street Name: </label>
+          <input name='street_name' type='text' onChange={this.handleChange} />
+          <br></br>
+
+          <label for='city'> City: </label>
+          <input name='city' type='text' onChange={this.handleChange} />
+          <br></br>
+
+          <label for='postal_code'> Postal Code: </label>
+          <input name='postal_code' type='text' onChange={this.handleChange} />
+          <br></br>
+
+          <label for="term">Choose Your Term: </label>
+          <select name="term" id="term" onChange={this.handleChange}>
+            <option value="fall">Fall</option>
+            <option value="winter">Winter</option>
+            <option value="spring">Spring</option>
+          </select>
+          <br></br>
+
+          <label for="start_date">Start Date: </label>
+          <input type="date" id="start" name="start_date"
+            value="2021-09-01"
+            min="2021-05-01" max="2022-08-31" onChange={this.handleChange}></input>
+          <br></br>
+
+          <label for="end_date">End Date: </label>
+          <input type="date" id="end" name="end_date"
+            value="2021-12-31"
+            min="2021-05-01" max="2022-08-31" onChange={this.handleChange}></input>
+          <br></br>
+
+          <label for='price_per_month'> Price Per Month: </label>
+          <input name='price_per_month' type='text' onChange={this.handleChange} />
+          <br></br>
+
+          {/* 
         <label for="is_negotiable"> Is it Negotiable?: </label>
         <select name="is_negotiable" id="is_negotiable">
           <option value="Yes">Yes</option>
@@ -48,23 +108,23 @@ const CreatePosting = (props) => {
         <br></br>
         */}
 
-        <label for="genders"> Who can live here?: </label>
-        <select name="genders" id="genders">
-          <option value="Female">Female</option>
-          <option value="Male">Male</option>
-          <option value="Co-ed">Co-ed</option>
-        </select>
-        <br></br>
+          <label for="gender_details"> Who can live here?: </label>
+          <select name="gender_details" id="genders" onChange={this.handleChange}>
+            <option value="female">Female</option>
+            <option value="male">Male</option>
+            <option value="co-ed">Co-ed</option>
+          </select>
+          <br></br>
 
-        <label for='number_rooms_open'> Number of Rooms Available: </label>
-        <input name='number_rooms_open' type='text' />
-        <br></br>
+          <label for='rooms_available'> Number of Rooms Available: </label>
+          <input name='rooms_available' type='text' onChange={this.handleChange} />
+          <br></br>
 
-        <label for='number_rooms_total'> Number of Rooms Total: </label>
-        <input name='number_rooms_total' type='text' />
-        <br></br>
+          <label for='total_rooms'> Number of Rooms Total: </label>
+          <input name='total_rooms' type='text' onChange={this.handleChange} />
+          <br></br>
 
-        {/* 
+          {/* 
         <label for='num_washrooms'> Number of Washrooms: </label>
         <input name='num_washrooms' type='text' />
         <br></br>
@@ -100,17 +160,18 @@ const CreatePosting = (props) => {
         <br></br>
 
         */}
-        <label for='description'> Description: </label>
-        <br></br>
-        <textarea rows="5" cols="60" name="description">
-          Enter More Details Here...
-        </textarea>
-        <br></br>
-        <br></br>
-        <input type="submit" value="submit" />
-      </form>
-    </Wrapper>
-  );
+          <label for='description'> Description: </label>
+          <br></br>
+          <textarea rows="5" cols="60" name="description" onChange={this.handleChange}>
+            Enter More Details Here...
+          </textarea>
+          <br></br>
+          <br></br>
+          <input type="submit" value="submit" />
+        </form>
+      </Wrapper>
+    );
+  }
 }
 
 export default CreatePosting;
