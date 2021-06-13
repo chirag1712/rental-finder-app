1281156d96ce2ae7dfa24fdf6a8d2a9030c83f02
Anne Hu <46465219+annehu2@users.noreply.github.com>
2021-06-13 12:55:21 -0400

    Organized Front-End (#21)
    * Reorganizing
    
    * Organized into components
    
    * Added router dom + styled components

---
+++ client/src/components/CreatePosting/CreatePosting.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Wrapper, Header } from './CreatePostingStyles';
+
+const CreatePosting = (props) => {
+  return(
+    <Wrapper>
+    <Header> Create Posting! </Header>
+    </Wrapper>
+  );
+}
+
+export default CreatePosting;
