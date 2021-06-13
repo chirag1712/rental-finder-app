f1b4cfd2a12719889a932de7698d225175acce65
Rain Zhao <rainzhao2000@gmail.com>
2021-06-13 12:59:57 -0400

    rain-17 trigger constraints (#24)
    * rain-17 triggers
    
    * rain-17 added error messages to trigger constraints
    
    * rain-17 moved triggers to its own sql file
    
    * rain-17 auto incr and check constraints
    
    * rain-17 changed indent to 2 spaces

--- server/database/db_helper.js
+++ server/database/db_helper.js
@@ -3,49 +3,51 @@ const { createReadStream, createWriteStream } = require('fs');
 const { spawn } = require('child_process');
 
 exports.exec_sql_script = (path, args=[]) => {
-    return new Promise((resolve, reject) => {
-        const mysql = spawn(
-            process.env.MYSQL_PATH,
-            [
-                `--host=${dbConfig.HOST}`,
-                `--user=${dbConfig.USER}`,
-                `--password=${dbConfig.PASSWORD}`,
-                ...args
-            ]
-        );
-        const stream = createReadStream(path);
-        stream.pipe(mysql.stdin);
-        mysql.stdout.on('data', data => {
-            console.log(`${data}`);
-        });
-        mysql.stderr.on('data', data => {
-            console.error(`${data}`);
-        });
-        mysql.on('exit', (code, signal) => {
-            resolve({ code, signal });
-        });
+  console.log(`Executing ${path}`);
+  return new Promise((resolve, reject) => {
+    const mysql = spawn(
+      process.env.MYSQL_PATH,
+      [
+        `--host=${dbConfig.HOST}`,
+        `--user=${dbConfig.USER}`,
+        `--password=${dbConfig.PASSWORD}`,
+        ...args
+      ]
+    );
+    const stream = createReadStream(path);
+    stream.pipe(mysql.stdin);
+    mysql.stdout.on('data', data => {
+      console.log(`${data}`);
     });
+    mysql.stderr.on('data', data => {
+      console.error(`${data}`);
+    });
+    mysql.on('exit', (code, signal) => {
+      resolve({ code, signal });
+    });
+  });
 }
 
 exports.dump = (path, args=[]) => {
-    return new Promise((resolve, reject) => {
-        const mysqldump = spawn(
-            process.env.MYSQLDUMP_PATH,
-            [
-                `--host=${dbConfig.HOST}`,
-                `--user=${dbConfig.USER}`,
-                `--password=${dbConfig.PASSWORD}`,
-                dbConfig.DB,
-                ...args
-            ]
-        );
-        const stream = createWriteStream(path);
-        mysqldump.stdout.pipe(stream);
-        mysqldump.stderr.on('data', data => {
-            console.error(`${data}`);
-        });
-        mysqldump.on('exit', (code, signal) => {
-            resolve({ code, signal });
-        });
+  console.log(`Dumping to ${path}`);
+  return new Promise((resolve, reject) => {
+    const mysqldump = spawn(
+      process.env.MYSQLDUMP_PATH,
+      [
+        `--host=${dbConfig.HOST}`,
+        `--user=${dbConfig.USER}`,
+        `--password=${dbConfig.PASSWORD}`,
+        dbConfig.DB,
+        ...args
+      ]
+    );
+    const stream = createWriteStream(path);
+    mysqldump.stdout.pipe(stream);
+    mysqldump.stderr.on('data', data => {
+      console.error(`${data}`);
     });
+    mysqldump.on('exit', (code, signal) => {
+      resolve({ code, signal });
+    });
+  });
 }
--- server/database/migrations/create_tables.sql
+++ server/database/migrations/create_tables.sql
@@ -1,48 +1,49 @@
 USE honkForSublet;
 
 CREATE TABLE User(
-    user_id INTEGER NOT NULL PRIMARY KEY,
-    email VARCHAR(100) NOT NULL UNIQUE,
-    password CHAR(64) NOT NULL, -- SHA-256
-    first_name VARCHAR(20),
-    last_name VARCHAR(20),
-    phone_num VARCHAR(20),
-    avatar TEXT
+  user_id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  email VARCHAR(100) NOT NULL UNIQUE,
+  password CHAR(64) NOT NULL, -- SHA-256
+  first_name VARCHAR(20),
+  last_name VARCHAR(20),
+  phone_num VARCHAR(20),
+  avatar TEXT
 );
 
 CREATE TABLE Posting(
-    posting_id INTEGER NOT NULL PRIMARY KEY,
-    user_id INTEGER NOT NULL REFERENCES User(user_id) ON DELETE CASCADE,
-    term SET('fall', 'winter', 'spring'),
-    start_date DATE NOT NULL,
-    end_date DATE NOT NULL,
-    pop INTEGER,
-    price_per_month SMALLINT NOT NULL,
-    gender_details ENUM('male', 'female', 'co-ed'),
-    rooms_available TINYINT,
-    total_rooms TINYINT,
-    description TEXT,
-    created_at DATETIME NOT NULL,
-    updated_at DATETIME NOT NULL
+  posting_id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  user_id INTEGER NOT NULL REFERENCES User(user_id) ON DELETE CASCADE,
+  term SET('fall', 'winter', 'spring'),
+  start_date DATE NOT NULL,
+  end_date DATE NOT NULL,
+  pop INTEGER,
+  price_per_month SMALLINT NOT NULL CHECK(price_per_month >= 0),
+  gender_details ENUM('male', 'female', 'co-ed'),
+  rooms_available TINYINT CHECK(rooms_available >= 0),
+  total_rooms TINYINT CHECK(total_rooms >= 0),
+  description TEXT,
+  created_at DATETIME NOT NULL,
+  updated_at DATETIME NOT NULL,
+  CHECK(end_date >= start_date)
 );
 
 CREATE TABLE PostingPhoto(
-    photo_id INTEGER NOT NULL PRIMARY KEY,
-    posting_id INTEGER NOT NULL REFERENCES Posting(posting_id) ON DELETE SET NULL,
-    url TEXT NOT NULL
+  photo_id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  posting_id INTEGER NOT NULL REFERENCES Posting(posting_id) ON DELETE SET NULL,
+  url TEXT NOT NULL
 );
 
 CREATE TABLE Address(
-    address_id INTEGER NOT NULL PRIMARY KEY,
-    city VARCHAR(20) NOT NULL,
-    street_name VARCHAR(20) NOT NULL,
-    street_num SMALLINT NOT NULL,
-    postal_code CHAR(6) NOT NULL UNIQUE,
-    building_name VARCHAR(20)
+  address_id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  city VARCHAR(20) NOT NULL,
+  street_name VARCHAR(20) NOT NULL,
+  street_num SMALLINT NOT NULL,
+  postal_code CHAR(6) NOT NULL UNIQUE,
+  building_name VARCHAR(20)
 );
 
 CREATE TABLE AddressOf(
-    posting_id INTEGER NOT NULL REFERENCES Posting(posting_id) ON DELETE CASCADE,
-    address_id INTEGER NOT NULL REFERENCES Address(address_id) ON DELETE CASCADE,
-    PRIMARY KEY(posting_id, address_id)
+  posting_id INTEGER NOT NULL REFERENCES Posting(posting_id) ON DELETE CASCADE,
+  address_id INTEGER NOT NULL REFERENCES Address(address_id) ON DELETE CASCADE,
+  PRIMARY KEY(posting_id, address_id)
 );
