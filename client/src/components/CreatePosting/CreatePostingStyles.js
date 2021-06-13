d6fd0adafac8f2e5648564017a19f5931444abe1
Anne Hu <annehuu2@gmail.com>
2021-06-12 20:43:32 -0400

    Organized into components

---
+++ client/src/components/CreatePosting/CreatePostingStyles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+height: 100%;
+width:100%;
+background-color: lightblue;
+`;
+
+export const Header = styled.h1`
+margin-top:0;
+`
