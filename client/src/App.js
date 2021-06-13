56e69c2f390bb7d95110c8d81ecc1b6348ee8803
Anne Hu <annehuu2@gmail.com>
2021-06-12 20:35:11 -0400

    Reorganizing

--- client/src/App.js
+++ client/src/App.js
@@ -1,82 +1,19 @@
-import './App.css';
-import { useState, useEffect } from 'react';
-import axios from 'axios';
+import './styles/App.css'
+import CreateUsers from './components/CreateUsers/CreateUsers';
+import CreatePosting from './components/CreatePosting/CreatePosting';
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 
 function App() {
-  const [studentInfo, setStudentInfo] = useState({
-    email: '', name: ''
-  });
-
-  const [studentList, setStudentList] = useState([]);
-
-  const [error, setError] = useState('');
-
-  useEffect(() => {
-    const func = async () => {
-      const res = await axios.get('/students');
-      //needs to be checked once DB is setup
-      setStudentList([...res.data]);
-    }
-
-    func()
-  }, []);
-
-  const onChange = (e) => setStudentInfo({...studentInfo, [e.target.name]: e.target.value});
-
-  const onSubmit = async () => {
-
-    const info = {...studentInfo};
-
-    if(info.name === '' || info.email === '') {
-      setError('Please enter all the fields');
-      return;
-    }
-
-    const regexLaurier = /^[\w.+\-]+@mylaurier\.ca$/;
-    const regexWaterloo = /^[\w.+\-]+@uwaterloo\.ca$/;
-    if (!regexLaurier.test(info.email) && !regexWaterloo.test(info.email)) {
-      setError('Please enter your valid university email');
-      return;
-    }
-
-    try {
-      const res = await axios.post('/students', info);
-    } catch(err) {
-      console.log(err.response.data);
-    }
-    
-    setStudentList([...studentList, info]);
-  };
 
   return (
-    <div className="App">
-
-      <input 
-        className='input'
-        type='text'
-        name='email'
-        placeholder='Student Email'
-        value={studentInfo.email}
-        onChange={e => onChange(e)}/>
-      <input 
-        className='input'
-        type='text'
-        name='name'
-        placeholder='Full Name'
-        value={studentInfo.name}
-        onChange={e => onChange(e)}/>
-      <button className="btn" onClick={onSubmit} >Submit</button>
-      <br/>
-      {(error!=='') && <span>{error}</span>}
-      <br/>
-      <br/>
-      <ul>
-      {
-        studentList.map(student => <li>{student.name}</li>)
-      }
-      </ul>
-    </div>
-  );
+    <div class="App">
+      <Router>
+        <Switch>
+          <Route path="/" exact component={CreateUsers} />
+          <Route path="/createPosting" component={CreatePosting} />
+        </Switch>
+      </Router>
+    </div>)
 }
 
 export default App;
