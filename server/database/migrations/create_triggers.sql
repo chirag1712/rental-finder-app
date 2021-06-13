44a8ad8c675eef7c890c248cffde9a9793e4fa59
rainzhao2000 <rainzhao2000@gmail.com>
2021-06-13 12:04:17 -0400

    rain-17 changed indent to 2 spaces

--- server/database/migrations/create_triggers.sql
+++ server/database/migrations/create_triggers.sql
@@ -4,14 +4,14 @@ DELIMITER //
 CREATE TRIGGER UserPostingUpperBound BEFORE INSERT ON Posting
 FOR EACH ROW
 BEGIN
-    IF
-        (SELECT COUNT(*)
-        FROM Posting AS p
-        WHERE p.user_id = NEW.user_id) >= 3
-    THEN
-        SIGNAL SQLSTATE '45000'
-        SET MESSAGE_TEXT = 'Exceeded number of postings per user (3)';
-    END IF;
+  IF
+    (SELECT COUNT(*)
+    FROM Posting AS p
+    WHERE p.user_id = NEW.user_id) >= 3
+  THEN
+    SIGNAL SQLSTATE '45000'
+    SET MESSAGE_TEXT = 'Exceeded number of postings per user (3)';
+  END IF;
 END;//
 DELIMITER ;
 
@@ -19,13 +19,13 @@ DELIMITER //
 CREATE TRIGGER PostingPhotoUpperBound BEFORE INSERT ON PostingPhoto
 FOR EACH ROW
 BEGIN
-    IF
-        (SELECT COUNT(*)
-        FROM PostingPhoto AS ph
-        WHERE ph.posting_id = NEW.posting_id) >= 10
-    THEN
-        SIGNAL SQLSTATE '45000'
-        SET MESSAGE_TEXT = 'Exceeded number of photos per posting (10)';
-    END IF;
+  IF
+    (SELECT COUNT(*)
+    FROM PostingPhoto AS ph
+    WHERE ph.posting_id = NEW.posting_id) >= 10
+  THEN
+    SIGNAL SQLSTATE '45000'
+    SET MESSAGE_TEXT = 'Exceeded number of photos per posting (10)';
+  END IF;
 END;//
 DELIMITER ;
