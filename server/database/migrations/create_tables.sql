e5ca92ba47a4bb3a8e3b57a70097ccd85ce12e79
rainzhao2000 <rainzhao2000@gmail.com>
2021-06-13 00:24:25 -0400

    rain-17 added error messages to trigger constraints

--- server/database/migrations/create_tables.sql
+++ server/database/migrations/create_tables.sql
@@ -56,7 +56,8 @@ BEGIN
         FROM Posting AS p
         WHERE p.user_id = NEW.user_id) >= 3
     THEN
-        SIGNAL SQLSTATE '45000';
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Exceeded number of postings per user (3)';
     END IF;
 END;//
 DELIMITER ;
@@ -70,7 +71,8 @@ BEGIN
         FROM PostingPhoto AS ph
         WHERE ph.posting_id = NEW.posting_id) >= 10
     THEN
-        SIGNAL SQLSTATE '45000';
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Exceeded number of photos per posting (10)';
     END IF;
 END;//
 DELIMITER ;
