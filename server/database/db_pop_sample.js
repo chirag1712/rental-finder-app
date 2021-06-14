b6abbaec0616fbb443a968bca8f547e7ab126b51
rainzhao2000 <rainzhao2000@gmail.com>
2021-06-14 03:41:34 -0400

    rain-25 bamboo scraper proof of concept

--- server/database/db_pop_sample.js
+++ server/database/db_pop_sample.js
@@ -40,7 +40,7 @@ const fb_scraper = async () => {
           await page.evaluate((sel, ind) => {
             const el = document.querySelectorAll(sel)[ind];
             el.click();
-          }, item_selector, pid + concurrency * i);
+          }, item_selector, index);
           // console.log(`Item clicked`);
           await page.waitForSelector(price_selector);
           // console.log(`Item page loaded`);
@@ -62,4 +62,66 @@ const fb_scraper = async () => {
   }
 };
 
-fb_scraper();
+const bamboo_scraper = async () => {
+  try {
+    const url = 'https://bamboohousing.ca/homepage';
+    const item_selector = '.ui.items>.item>.desktoplisting';
+    const start_date_selector = '.calendar~.content>.description';
+    const duration_selector = '.clock~.content>.description';
+    const price_selector = '.ui.segment>h2.ui.header';
+    const gender_selector = '.man~.content>.description';
+    const rooms_available_selector = '.hashtag~.content>.description';
+    const description_selector = '.listingdescription';
+    // const address_selector = 'body>div>div>div>div>div>div>div>div>div>div>div>div>div>div>div>div>div>div>div>div>div>div>div>div>div>div>span';
+    // const browser = await puppeteer.launch({ devtools: true });
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(url, { timeout: 60000 });
+    // console.log(`Connected to ${url}`);
+    await page.waitForSelector(item_selector);
+    // console.log(`Page has loaded`);
+    //save target of original page to know that this was the opener: 
+    const pageTarget = page.target();
+    const listLen = await page.evaluate(
+      sel => document.querySelector(sel).parentElement.parentElement.childElementCount,
+      item_selector
+    );
+    for (let index = 0; index < listLen; ++index) {
+      await page.evaluate((sel, ind) => {
+        const el = document.querySelectorAll(sel)[ind];
+        el.click();
+      }, item_selector, index);
+      // console.log(`Item clicked`);
+      //check that the first page opened this new page:
+      const newTarget = await browser.waitForTarget(target => target.opener() === pageTarget);
+      //get the new page object:
+      const newPage = await newTarget.page();
+      await newPage.waitForNavigation({ waitUntil: 'networkidle0' });
+      // console.log(`Item page loaded`);
+      const data = await newPage.evaluate(
+        (
+          start_date_selector, duration_selector, price_selector, gender_selector,
+          rooms_available_selector, description_selector
+        ) => {
+          return {
+            start_date: document.querySelector(start_date_selector).innerText,
+            duration: document.querySelector(duration_selector).innerText,
+            price: document.querySelectorAll(price_selector)[1].innerText,
+            gender: document.querySelector(gender_selector).innerText,
+            rooms_available: document.querySelector(rooms_available_selector).innerText,
+            description: document.querySelector(description_selector).innerText
+          };
+        },
+        start_date_selector, duration_selector, price_selector, gender_selector,
+        rooms_available_selector, description_selector
+      );
+      console.log(data);
+      await newPage.close();
+    }
+    await browser.close();
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+bamboo_scraper();
