7ce18e28b3e4def4310c2bca3144f265117917ad
uday <usbenipa@uwaterloo.ca>
2021-06-14 17:37:30 -0400

    createPosting route protection and conversion of createposting class to functional component

--- client/src/components/CreatePosting/CreatePosting.js
+++ client/src/components/CreatePosting/CreatePosting.js
@@ -1,10 +1,14 @@
-import React, { Component } from 'react';
-import { Wrapper, Header } from './CreatePostingStyles';
-import axios from 'axios';
-import { render } from 'react-dom';
+import { useState, useEffect } from 'react';
+import { useHistory } from "react-router-dom";
 
-class CreatePosting extends Component {
-  state = {
+import axios from 'axios';
+
+import { Wrapper, Header } from './CreatePostingStyles';
+
+const CreatePosting = ({ user_id }) => {
+  const history = useHistory();
+
+  const [info, setInfo] = useState({
     term: '',
     start_date: '',
     end_date: '',
@@ -17,115 +21,101 @@ class CreatePosting extends Component {
     street_name: '',
     city: '',
     postal_code: ''
-  }
+  });
 
-  handleChange = event => {
-    this.setState({ [event.target.name]: event.target.value })
-  }
+  useEffect(() => {
+    if(user_id==null) history.push('/');
+  }, []);
 
-  handleSubmit = async (event) => {
-    event.preventDefault();
+  const handleChange = e => setInfo({ ...info, [e.target.name]: e.target.value })
 
-    const data = {
-      user_id: this.props.user_id,
-      term: this.state.term,
-      start_date: this.state.start_date,
-      end_date: this.state.end_date,
-      price_per_month: this.state.price_per_month,
-      gender_details: this.state.gender_details,
-      rooms_available: this.state.rooms_available,
-      total_rooms: this.state.total_rooms,
-      description: this.state.description,
-      street_num: this.state.street_num,
-      street_name: this.state.street_name,
-      city: this.state.city,
-      postal_code: this.state.postal_code
-    };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const data = { user_id, ...info };
 
     try {
       const response = await axios.post('api/postings/create', data);
       alert('Successfully Made a Posting!');
-      console.log(response.data);
+      // console.log(response.data);
     } catch (err) {
       alert('Error!');
       console.log(err.response.data);
     }
   }
 
-  render() {
-    return (
-      <Wrapper>
-        <Header> Create Posting! </Header>
+  return (
+    <Wrapper>
+      <Header> Create Posting! </Header>
 
-        <form action="" onSubmit={this.handleSubmit}>
-          <label for='street_num'> Street Number: </label>
-          <input name='street_num' type='text' onChange={this.handleChange} />
-          <br></br>
+      <form action="" onSubmit={handleSubmit}>
+        <label for='street_num'> Street Number: </label>
+        <input name='street_num' type='text' onChange={handleChange} />
+        <br></br>
 
-          <label for='street_name'> Street Name: </label>
-          <input name='street_name' type='text' onChange={this.handleChange} />
-          <br></br>
+        <label for='street_name'> Street Name: </label>
+        <input name='street_name' type='text' onChange={handleChange} />
+        <br></br>
 
-          <label for='city'> City: </label>
-          <input name='city' type='text' onChange={this.handleChange} />
-          <br></br>
+        <label for='city'> City: </label>
+        <input name='city' type='text' onChange={handleChange} />
+        <br></br>
 
-          <label for='postal_code'> Postal Code: </label>
-          <input name='postal_code' type='text' onChange={this.handleChange} />
-          <br></br>
+        <label for='postal_code'> Postal Code: </label>
+        <input name='postal_code' type='text' onChange={handleChange} />
+        <br></br>
 
-          <label for="term">Choose Your Term: </label>
-          <select name="term" id="term" onChange={this.handleChange}>
-            <option value="" disabled selected>Select</option>
-            <option value="fall">Fall</option>
-            <option value="winter">Winter</option>
-            <option value="spring">Spring</option>
-          </select>
-          <br></br>
+        <label for="term">Choose Your Term: </label>
+        <select name="term" id="term" onChange={handleChange}>
+          <option value="" disabled selected>Select</option>
+          <option value="fall">Fall</option>
+          <option value="winter">Winter</option>
+          <option value="spring">Spring</option>
+        </select>
+        <br></br>
 
-          <label for="start_date">Start Date: </label>
-          <input type="date" id="start" name="start_date"
-            min="2021-05-01" max="2022-08-31" onChange={this.handleChange}></input>
-          <br></br>
+        <label for="start_date">Start Date: </label>
+        <input type="date" id="start" name="start_date"
+          min="2021-05-01" max="2022-08-31" onChange={handleChange}></input>
+        <br></br>
 
-          <label for="end_date">End Date: </label>
-          <input type="date" id="end" name="end_date"
-            min="2021-05-01" max="2022-08-31" onChange={this.handleChange}></input>
-          <br></br>
+        <label for="end_date">End Date: </label>
+        <input type="date" id="end" name="end_date"
+          min="2021-05-01" max="2022-08-31" onChange={handleChange}></input>
+        <br></br>
 
-          <label for='price_per_month'> Price Per Month: </label>
-          <input name='price_per_month' type='text' onChange={this.handleChange} />
-          <br></br>
+        <label for='price_per_month'> Price Per Month: </label>
+        <input name='price_per_month' type='text' onChange={handleChange} />
+        <br></br>
 
-          <label for="gender_details"> Who can live here?: </label>
-          <select name="gender_details" id="gender_details" onChange={this.handleChange}>
-            <option value="" disabled selected>Select</option>
-            <option value="female">Female</option>
-            <option value="male">Male</option>
-            <option value="co-ed">Co-ed</option>
-          </select>
-          <br></br>
+        <label for="gender_details"> Who can live here?: </label>
+        <select name="gender_details" id="gender_details" onChange={handleChange}>
+          <option value="" disabled selected>Select</option>
+          <option value="female">Female</option>
+          <option value="male">Male</option>
+          <option value="co-ed">Co-ed</option>
+        </select>
+        <br></br>
 
-          <label for='rooms_available'> Number of Rooms Available: </label>
-          <input name='rooms_available' type='text' onChange={this.handleChange} />
-          <br></br>
+        <label for='rooms_available'> Number of Rooms Available: </label>
+        <input name='rooms_available' type='text' onChange={handleChange} />
+        <br></br>
 
-          <label for='total_rooms'> Number of Rooms Total: </label>
-          <input name='total_rooms' type='text' onChange={this.handleChange} />
-          <br></br>
+        <label for='total_rooms'> Number of Rooms Total: </label>
+        <input name='total_rooms' type='text' onChange={handleChange} />
+        <br></br>
 
-          <label for='description'> Description: </label>
-          <br></br>
-          <textarea rows="5" cols="60" name="description" onChange={this.handleChange}>
-            Enter More Details Here...
-          </textarea>
-          <br></br>
-          <br></br>
-          <input type="submit" value="submit" />
-        </form>
-      </Wrapper>
-    );
-  }
+        <label for='description'> Description: </label>
+        <br></br>
+        <textarea rows="5" cols="60" name="description" onChange={handleChange}>
+          Enter More Details Here...
+        </textarea>
+        <br></br>
+        <br></br>
+        <input type="submit" value="submit" />
+      </form>
+    </Wrapper>
+  );
 }
 
 export default CreatePosting;
