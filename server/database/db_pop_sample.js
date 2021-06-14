893b76d6e38b3da7e8c0e11bf1cb6eebec528c46
rainzhao2000 <rainzhao2000@gmail.com>
2021-06-14 01:58:15 -0400

    rain-25 fixed fb scraper

--- server/database/db_pop_sample.js
+++ server/database/db_pop_sample.js
@@ -1,88 +1,65 @@
 const puppeteer = require('puppeteer');
 const { Cluster } = require('puppeteer-cluster');
 
-(async () => {
+const fb_scraper = async () => {
   try {
     const url = 'https://www.facebook.com/marketplace/112763262068685/propertyrentals';
     const item_selector = 'div>div>div>span>div>div>a[role="link"][tabindex="0"]';
-    const address_selector = 'body>div>div>div>div>div>div>div>div>div>div>div>div>div>div>div>div>div>div>div>div>div>div>div>div>div>div>span';
-    // const url = 'https://bamboohousing.ca/homepage';
-    // const item_selector = '.ui.items .item .desktoplisting';
-
-    // const browser = await puppeteer.launch({ headless: false, devtools: true });
-    // const browser = await puppeteer.launch();
-    // const page = await browser.newPage();
-    // await page.goto(url);
-    // console.log(`Connected to ${url}`);
-
-    // await page.waitForSelector(item_selector);
-    // await page.evaluate(sel => {
-    //   const el = document.querySelectorAll(sel)[0];
-    //   el.click();
-    // }, item_selector);
-
-    // await page.waitForSelector(address_selector);
-    // const address = await page.evaluate(sel => document.querySelector(sel).innerText, address_selector);
-    // console.log(address);
-
-    // await page.goBack();
-    // await page.waitFor(2000);
-
+    // const address_selector = 'body>div>div>div>div>div>div>div>div>div>div>div>div>div>div>div>div>div>div>div>div>div>div>div>div>div>div>span';
+    const price_selector = 'body>div>div>div>div>div>div>div>div>div>div>div>div>div>div>div>div>div>div>div>div>div>span';
     const concurrency = 5;
     const cluster = await Cluster.launch({
       concurrency: Cluster.CONCURRENCY_CONTEXT,
       maxConcurrency: concurrency,
+      timeout: 60000 // ms
     });
-    await cluster.task(async ({ page, data: { pid, url, item_selector, address_selector } }) => {
-      for (let i = 1; i <= 10; ++i) { // 5 crawlers * 10 posts = 50 posts
-        await page.goto(url);
-        console.log(`Connected to ${url}`);
-        await page.waitForSelector(item_selector);
-        if (i > 3) { // start scrolling to load more posts
-          const oldLen = await page.evaluate(sel => {
-            return document.querySelector(sel)
-            .parentElement.parentElement.parentElement
-            .parentElement.parentElement.parentElement
-            .childElementCount;
-          }, item_selector);
-          await page.evaluate(() => window.scrollTo(0, document.body.scrollHeight));
-          await page.evaluate(async (sel, oldLen) => {
+    await cluster.task(async ({ page, data: { concurrency, pid, url, item_selector, price_selector } }) => {
+      const numPostsPerScraper = 50 / concurrency;
+      const scrollThreshold = 20 / concurrency;
+      await page.goto(url, { timeout: 0 });
+      // console.log(`Connected to ${url}`);
+      for (let i = 0; i < numPostsPerScraper; ++i) {
+        const index = pid + concurrency * i;
+        try {
+          await page.waitForSelector(item_selector);
+          await page.evaluate(async () => {
             return await new Promise(resolve => {
-              const checkLoaded = setInterval(resolve => {
-                const newLen = document.querySelector(sel)
-                .parentElement.parentElement.parentElement
-                .parentElement.parentElement.parentElement
-                .childElementCount;
-                if (newLen > oldLen) {
-                  clearInterval(checkLoaded);
+              let it = 0;
+              let scrollInterval;
+              const scroll = resolve => {
+                window.scrollTo(0, document.body.scrollHeight);
+                if (++it > 1) {
+                  clearInterval(scrollInterval);
                   resolve();
-                  return;
                 }
-              }, 0, resolve);
+              }
+              scrollInterval = setInterval(scroll, 100, resolve);
             });
-          }, item_selector, oldLen);
+          });
+          // console.log(`Page has loaded`);
+          await page.evaluate((sel, ind) => {
+            const el = document.querySelectorAll(sel)[ind];
+            el.click();
+          }, item_selector, pid + concurrency * i);
+          // console.log(`Item clicked`);
+          await page.waitForSelector(price_selector);
+          // console.log(`Item page loaded`);
+          const price = await page.evaluate(sel => document.querySelector(sel).innerText, price_selector);
+          await page.goBack();
+          console.log({pid, i, index, price});
+        } catch (err) {
+          console.error({pid, i, index, err});
         }
-        console.log(`Page has loaded`);
-        await page.evaluate((sel, ind) => {
-          const el = document.querySelectorAll(sel)[ind];
-          el.click();
-        }, item_selector, pid * i);
-        console.log(`Item clicked`);
-        await page.waitForSelector(address_selector);
-        console.log(`Item page loaded`);
-        const address = await page.evaluate(sel => document.querySelector(sel).innerText, address_selector);
-        console.log(address);
-        await page.goBack();
       }
     });
-    for (let pid = 0; pid < concurrency; ++pid) {
-      cluster.queue({ pid, url, item_selector, address_selector });
+    for (let pid = 1; pid <= concurrency; ++pid) {
+      cluster.queue({ concurrency, pid, url, item_selector, price_selector });
     }
     await cluster.idle();
     await cluster.close();
-
-    // await browser.close();
   } catch (err) {
     console.error(err);
   }
-})();
+};
+
+fb_scraper();
