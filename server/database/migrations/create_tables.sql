881efd7ec368c7a1a2460cd827863b773c3eb09e
Parmeet Chawla <ps3chawl@uwaterloo.ca>
2021-07-04 15:06:46 -0400

    bug-fix

--- server/database/migrations/create_tables.sql
+++ server/database/migrations/create_tables.sql
@@ -29,7 +29,7 @@ CREATE TABLE Posting(
   description TEXT,
   created_at DATETIME NOT NULL,
   updated_at DATETIME NOT NULL,
-  CONSTRAINT rooms_available_less_than_total_rooms CHECK(rooms_available >= total_rooms),
+  CONSTRAINT rooms_available_less_than_total_rooms CHECK(rooms_available <= total_rooms),
   CONSTRAINT end_date_after_start_date CHECK(end_date >= start_date),
   CONSTRAINT updated_at_after_created_at CHECK(updated_at >= created_at)
 ) DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci;
