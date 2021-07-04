cc1f3ffaa54dbfa4babb4745f56ed4f8bc51f359
Chirag <35068753+chirag1712@users.noreply.github.com>
2021-07-05 04:18:17 +0530

    minor (#63)
    Added create indices script back to the setup script

--- server/database/db_setup.js
+++ server/database/db_setup.js
@@ -5,6 +5,7 @@ const { exec_sql_script } = require('./db_helper.js');
     await exec_sql_script('database/migrations/drop_db.sql');
     await exec_sql_script('database/migrations/create_db.sql');
     await exec_sql_script('database/migrations/create_tables.sql');
+    await exec_sql_script('database/migrations/create_indices.sql');
     await exec_sql_script('database/migrations/create_triggers.sql');
   } catch (error) {
     console.error(error);
