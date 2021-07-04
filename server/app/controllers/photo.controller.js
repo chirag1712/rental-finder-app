32b8a702d3f6cd1e7077c53871be56c3562295d2
chirag1712 <chiragjindal1712@gmail.com>
2021-07-05 01:56:05 +0530

    added photo uploading functionality to create posting endpoint

--- server/app/controllers/photo.controller.js
+++ server/app/controllers/photo.controller.js
@@ -1,39 +1,25 @@
 const Photo = require("../models/photo.model.js");
-var aws = require("aws-sdk");
-const util = require("util");
+const uploadToS3 = require("../models/s3.js");
 
+// used to add photos for existing postings
 const upload = async (request, response) => {
-    const files = request.files;
+    const files = request.files
+    const posting_id = request.body.posting_id
     const s3FileUrl = process.env.AWS_UPLOADED_FILE_URL_LINK;
 
-    let s3bucket = new aws.S3({
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-        region: process.env.AWS_REGION,
-    });
-
-    const upload = util.promisify(s3bucket.upload).bind(s3bucket);
     const photo_ids = [];
 
     const uploadPromises = files.map(async (file) => {
         try {
-            var key = request.body.posting_id + "_" + new Date().toISOString() + "_" + file.originalname;
-            var params = {
-                Bucket: process.env.AWS_BUCKET_NAME,
-                Key: key,
-                Body: file.buffer,
-                ContentType: file.mimetype,
-                ACL: "public-read",
-            };
-            const data = await upload(params);
+            var key = "posting_" + posting_id + "_" + new Date().toISOString() + "_" + file.originalname;
+            const data = await uploadToS3(key, file);
             if (!data) {
-                return response.status(401).json({ error: "Photo upload to s3 failed"});
+                return response.status(401).json({ error: "Photo upload to s3 failed" });
             }
-            console.log(data);
 
             // add image url to db
             const photo = new Photo({
-                posting_id: request.body.posting_id,
+                posting_id: posting_id,
                 photo_url: s3FileUrl + key,
             });
             const photo_id = await Photo.create(photo);
--- server/app/controllers/posting.controller.js
+++ server/app/controllers/posting.controller.js
@@ -1,112 +1,138 @@
 const moment = require('moment');
 const { Posting, Address, AddressOf } = require("../models/posting.model.js");
+const Photo = require("../models/photo.model.js");
+const uploadToS3 = require("../models/s3.js");
+
 //express validator
 const { check, validationResult } = require("express-validator");
 
 // Create a new posting
 const create = async (request, response) => {
-	// Validate request
-	if (!request.body) {
-		return response.status(400).send({
-			message: "Content can not be empty!",
-		});
-	}
+    // Validate request
+    if (!request.body) {
+        return response.status(400).send({
+            message: "Content can not be empty!",
+        });
+    }
 
+    // Create a Posting
+    const posting = new Posting({
+        user_id: request.body.user_id,
+        term: request.body.term,
+        start_date: request.body.start_date,
+        end_date: request.body.end_date,
+        price_per_month: request.body.price_per_month,
+        gender_details: request.body.gender_details,
+        rooms_available: request.body.rooms_available,
+        total_rooms: request.body.total_rooms,
+        ac: request.body.ac,
+        washrooms: request.body.washrooms,
+        wifi: request.body.wifi,
+        parking: request.body.parking,
+        laundry: request.body.laundry,
+        description: request.body.description,
+        created_at: moment().format('YYYY-MM-DD HH:mm:ss'),
+        updated_at: moment().format('YYYY-MM-DD HH:mm:ss'),
+    });
 
-  // Create a Posting
-  const posting = new Posting({
-    user_id: request.body.user_id,
-    term: request.body.term,
-    start_date: request.body.start_date,
-    end_date: request.body.end_date,
-    price_per_month: request.body.price_per_month,
-    gender_details: request.body.gender_details,
-    rooms_available: request.body.rooms_available,
-    total_rooms: request.body.total_rooms,
-    ac: request.body.ac,
-    washrooms: request.body.washrooms,
-    wifi: request.body.wifi,
-    parking: request.body.parking,
-    laundry: request.body.laundry,
-    description: request.body.description,
-    created_at: moment().format('YYYY-MM-DD HH:mm:ss'),
-    updated_at: moment().format('YYYY-MM-DD HH:mm:ss'),
-  });
+    // Create a Address (only if it doesn't exist)
+    const address = new Address({
+        street_num: request.body.street_num,
+        street_name: request.body.street_name,
+        city: request.body.city,
+        postal_code: request.body.postal_code,
+        building_name: request.body.building_name,
+    });
 
-	// Create a Address (only if it doesn't exist)
-	const address = new Address({
-		street_num: request.body.street_num,
-		street_name: request.body.street_name,
-		city: request.body.city,
-		postal_code: request.body.postal_code,
-		building_name: request.body.building_name,
-	});
+    try {
+        // validate that user exists in the db
+        const user = await Posting.userCheck(request.body.user_id)
+        if (!user) {
+            return result.status(400).json({ error: "User doesn't exist." });
+        }
 
-	try {
-		// validate that user exists in the db
-		const user = await Posting.userCheck(request.body.user_id)
-		if (!user) {
-			return response.status(400).json({ error: "User doesn't exist." });
-		}
+        // Save Posting in the database
+        const newPosting = await Posting.create(posting);
 
-		// Save Posting in the database
-		const newPosting = await Posting.create(posting);
+        // Address handling for the new posting
+        const foundAddress = await Address.search(address);
+        if (foundAddress[0]) {
+            const addressOf = new AddressOf({
+                posting_id: newPosting.posting_id,
+                address_id: foundAddress[0].address_id,
+            });
+            AddressOf.create(addressOf);
+        } else {
+            const newAddress = await Address.create(address);
+            const addressOf = new AddressOf({
+                posting_id: newPosting.posting_id,
+                address_id: newAddress.address_id,
+            });
+            AddressOf.create(addressOf);
+        }
 
-		// Address handling for the new posting
-		const foundAddress = await Address.search(address);
-		if (foundAddress[0]) {
-			const addressOf = new AddressOf({
-				posting_id: newPosting.posting_id,
-				address_id: foundAddress[0].address_id,
-			});
-			AddressOf.create(addressOf);
-		} else {
-			const newAddress = await Address.create(address);
-			const addressOf = new AddressOf({
-				posting_id: newPosting.posting_id,
-				address_id: newAddress.address_id,
-			});
-			AddressOf.create(addressOf);
-		}
+        // posting photos handling
+        const files = request.files;
+        const s3FileUrl = process.env.AWS_UPLOADED_FILE_URL_LINK;
 
-		response.status(200).json(posting);
-	} catch (err) {
-		// console.log(err);
-		return response.status(500).send({ error: "Internal server Error" })
-	}
+        const photo_ids = [];
+        const uploadPromises = files.map(async (file) => {
+            var key = "posting_" + newPosting.posting_id + "_" + new Date().toISOString() + "_" + file.originalname;
+            const data = await uploadToS3(key, file);
+            if (!data) {
+                return response.status(401).json({ error: "Photo upload to s3 failed" });
+            }
+
+            // add image url to db
+            const photo = new Photo({
+                posting_id: newPosting.posting_id,
+                photo_url: s3FileUrl + key,
+            });
+            const photo_id = await Photo.create(photo);
+            photo_ids.push(photo_id);
+        });
+        await Promise.all([...uploadPromises]);
+        posting.photo_ids = photo_ids;
+        console.log("photo_ids for posting = ", photo_ids);
+
+        return result.status(200).json(posting);
+    } catch (err) {
+        console.log(err);
+        return result.status(500).send({ error: "Internal server Error" })
+    }
 };
 
 
 const indexPostingsValidation = [
-	check("page", "Page Field Is A Number").not().isEmpty().isNumeric()
+    check("page", "Page Field Is A Number").not().isEmpty().isNumeric()
 ]
 
 const indexPostings = async (request, response) => {
 
-	// Validate request
-	const errors = validationResult(request);
-	if (!errors.isEmpty()) {
-		let errorArray = errors.array().map(e => e.msg);
-		return response.status(400).json({ error: errorArray[0] });
-	}
+    // Validate request
+    const errors = validationResult(request);
+    if (!errors.isEmpty()) {
+        let errorArray = errors.array().map(e => e.msg);
+        return response.status(400).json({ error: errorArray[0] });
+    }
 
-	try {
-		const postings = await Posting.getPostings(request.body)
+    try {
+        const postings = await Posting.getPostings(request.body)
 
-		let next = false;
-		if (postings.length > 20) {
-			postings.pop();
-			next = true;
-		}
+        let next = false;
+        if (postings.length > 20) {
+            postings.pop();
+            next = true;
+        }
 
-		response.status(200).json({ list: postings, next });
-	} catch (err) {
-		return response.status(500).send({ error: 'Internal server Error' })
-	}
+        response.status(200).json({ list: postings, next });
+    } catch (err) {
+        return response.status(500).send({ error: 'Internal server Error' })
+    }
 }
 
 module.exports = {
-	create,
-	indexPostingsValidation,
-	indexPostings
+    create,
+    indexPostingsValidation,
+    indexPostings
 }
